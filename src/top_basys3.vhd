13584575011d30c876ed83a235f0e7ec1699ab1c
goodpadhi <maheshpadhi222@gmail.com>
2026-03-12 20:38:25 -0600

    Push 26

--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -55,8 +55,8 @@
 --|
 --+----------------------------------------------------------------------------
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+  
 
 
 entity top_basys3 is
